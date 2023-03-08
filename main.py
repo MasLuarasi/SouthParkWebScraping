3b2007f34b87f50a1b32f45cf128f924b270e9af
MasLuarasi <87911434+MasLuarasi@users.noreply.github.com>
2023-03-08 14:56:11 -0500

    Optimized some of the code. Got rid of a couple for loops

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from better_profanity import profanity
+from collections import OrderedDict
+from collections import Counter
 import re
 import json
 import time
@@ -12,27 +14,18 @@ file.close()
 
 episodeNumber = 1
 
-def analyzeLine(line):#Count the number of words and profanities in the one line.
-    ret = [1]#1 as the first element representing the line. We also want to see how many lines each character has.
-    l = line.split(" ")#Split into list of words and add length of the split to list.
+def analyzeLine(line):#Count the number of words and profanities in the one line
+    ret = [1]#1 as the first element representing the line. We also want to see how many lines each character has
+    l = line.split(" ")#Split into list of words and add length of the split to list
     while("" in l):l.remove("")#Remove an empty elements in list that went through
-    ret.append(len(l))#Length of list represents number of words in line.
-    profanityCount = 0
-    profanityFrequency = dict()
+    ret.append(len(l))#Length of list represents number of words in line
+    profanityFrequency = []#Contains all the profanities for this line
     for word in l:#Library only checks if parameter has profanity, loop through split list from earlier to get total number
         if(profanity.contains_profanity(word) or (word[len(word)-1] == 's' and profanity.contains_profanity(word[:len(word)-1]))):#Some words in dictionary don't account for plural cases.
-            profanityCount += 1
-            if word in profanityFrequency:#If profanity has more than one occurence in the line
-                profanityFrequency[word] += 1#Increment the value for it by one
-            else:
-                profanityFrequency[word] = 1#Create a new entry and set it to one
-    ret.append(profanityCount)
-    ret.append(profanityFrequency)
+            profanityFrequency.append(word)#Add to list
+    ret.append(len(profanityFrequency))#Add number of profanities in this line
+    ret.append(profanityFrequency)#Add list of them
     return ret
-
-def calculateProfanityPercent(d):
-    for v in d.values():
-        v.append(round(v[2]/v[1] * 100, 2))#Add the result of the profanity count / total words. 
 
 for episode in episodeLinks:
     time.sleep(2)
@@ -57,7 +50,6 @@ for episode in episodeLinks:
 
     for d in dialogue:
         s = d
-        # print(s + "\n-------------------------------------------")
         while('[' in s):#Scene descriptions are in []. Since they are not dialogue, remove them so they don't count.
             start = s.find('[')
             end = s.find(']') + 1
@@ -65,9 +57,8 @@ for episode in episodeLinks:
                 s = s[:start]#Substring til the end cause all instances we've come across have it ending the text
             else:
                 s = ("".join([s[:start], s[end:]]))#Substring from start to opening of scene description, with end of scene description to end of string   s[:start] + s[end:]
-        # print(s)
-        lowered = s.lower().replace("-", " ")
-        removedPunctuation = re.sub(r'[^\w\s]', '', lowered)#Remove punctuation from stirng  FIGURE OUT BETTER WAY TO DEAL WITH HYPHENS REPLACE HYPENS WITH ' '
+        lowered = s.lower().replace("-", " ")#Replace hyphens with space so words or stutters(F Jimmy) don't get lost
+        removedPunctuation = re.sub(r'[^\w\s]', '', lowered)#Remove punctuation from stirng
 
         characterAndDialogue = removedPunctuation.split("\n")#Format result to just be character name and dialogue, no \n. Split at new line so character name is first and what they say is second.
         while("" in characterAndDialogue): 
@@ -81,21 +72,40 @@ for episode in episodeLinks:
             for i in range(0,3):
                 characterData[characterAndDialogue[0]][i] += lineWordProf[i]#Increment the existing elements by the data found in the current line
             if(lineWordProf[3] != None):#If the profanity frequency dictionary is not empty
-                for p in lineWordProf[3]:#For every word in the profanity frequency dictionary
-                    if (p in characterData[characterAndDialogue[0]][3]):#If that profanity already exists in that characters profanity frequency dictionary
-                        characterData[characterAndDialogue[0]][3][p] += 1#Increment the value for the key in the dictionary by one
-                    else:
-                        characterData[characterAndDialogue[0]][3][p] = 1#Create a new entry and set it to one
+                characterData[characterAndDialogue[0]][3] += lineWordProf[3]#Add the profanities from the line we just analyzed
         else:
             characterData[characterAndDialogue[0]] = lineWordProf#Create a new entry and set it to the data found in the current line
 
-    calculateProfanityPercent(characterData)
+    sortedCharacterDataByLines = sorted(characterData.items(), key=operator.itemgetter(1), reverse=True)#Sort dictionary so character with most lines is first
 
-    sortedCharacterDataByLines = sorted(characterData.items(), key=operator.itemgetter(1), reverse=True)#Sort the dictionary so that the characters with most lines are first
     for i in range(len(sortedCharacterDataByLines)):
-        sortedCharacterDataByLines[i][1][3] = dict(sorted(sortedCharacterDataByLines[i][1][3].items(), key=operator.itemgetter(1), reverse=True))#For each character, sort their profanity frequency dictionary so most common used ones are first 
+        t = sortedCharacterDataByLines[i][1]#Temp variable
+        t.append(round(t[2]/t[1] * 100, 2))#Append the frequency of profanities as a %. Profanities/Words
+        t[3] = OrderedDict(Counter(t[3]).most_common())#Count the frequency of each profanity. Replace the list in the character data with a dictionary that has profanity as key and frequency as value
 
     with open('Seasons\\Movie\\' + titleFile + '.json', 'w') as convert_file:
         convert_file.write(json.dumps(sortedCharacterDataByLines))
 
+
 # Bugs to fix
+#Write all p's to array, then at the part where we were working with Aadya count them and sort them.
+
+    # profanityFrequency = dict()
+    # for word in l:#Library only checks if parameter has profanity, loop through split list from earlier to get total number
+    #     if(profanity.contains_profanity(word) or (word[len(word)-1] == 's' and profanity.contains_profanity(word[:len(word)-1]))):#Some words in dictionary don't account for plural cases.
+    #         profanityCount += 1
+    #         if word in profanityFrequency:#If profanity has more than one occurence in the line
+    #             profanityFrequency[word] += 1#Increment the value for it by one
+    #         else:
+    #             profanityFrequency[word] = 1#Create a new entry and set it to one
+
+
+                # for p in lineWordProf[3]:#For every word in the profanity frequency dictionary
+                #     characterData[characterAndDialogue[0]][3].append(p)
+                    # if (p in characterData[characterAndDialogue[0]][3]):#If that profanity already exists in that characters profanity frequency dictionary
+                    #     characterData[characterAndDialogue[0]][3][p] += 1#Increment the value for the key in the dictionary by one
+                    # else:
+                    #     characterData[characterAndDialogue[0]][3][p] = 1#Create a new entry and set it to one
+
+    # for i in range(len(sortedCharacterDataByLines)):
+    #     sortedCharacterDataByLines[i][1][3] = dict(sorted(sortedCharacterDataByLines[i][1][3].items(), key=operator.itemgetter(1), reverse=True))#For each character, sort their profanity frequency dictionary so most common used ones are first 
