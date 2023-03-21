c3a177ba7841188a5df0cc1c1121b0e20f69cee3
MasLuarasi <87911434+MasLuarasi@users.noreply.github.com>
2023-03-21 17:50:59 -0400

    Fixed episode links

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from better_profanity import profanity
-from collections import OrderedDict
 from collections import Counter
 import re
 import json
 import time
 import operator
 
-file = open("Seasons\\26\\Links.txt", "r")
-episodeLinks = file.read().split("\n")
-file.close()
-
-episodeNumber = 1
-episodeTitles = []
+# st = time.time()
 
 def analyzeLine(line):#Count the number of words and profanities in the one line
     ret = [1]#1 as the first element representing the line. We also want to see how many lines each character has
@@ -28,11 +22,10 @@ def analyzeLine(line):#Count the number of words and profanities in the one line
     ret.append(profanityFrequency)#Add list of them
     return ret
 
-def computeSummary():
+def computeSummary():#Add all the data from each episode in the season
     seasonSummary = dict()
-
     for e in episodeTitles:#Subsequent episodes and their data
-        current =  json.load(open('Seasons\\26\\' + e + '.json'))#Get the data of this episode
+        current =  json.load(open('Seasons\\26\\' + e + '.json'))#Get the data of current episode
         for c in current:#For each key
             if(c in seasonSummary.keys()):#If character already has an entry in the dictionary, so if they were in one of previous episodes
                 for j in range(0,3):
@@ -44,12 +37,20 @@ def computeSummary():
 
     seasonSummary = dict(sorted(seasonSummary.items(), key=operator.itemgetter(1), reverse=True))#Sort dictionary so character with most lines is first
 
-    with open('Seasons\\26\\Summary.json', 'w') as convert_file:
-        convert_file.write(json.dumps(seasonSummary))
-        
+    with open('Seasons\\26\\Summary.json', 'w') as f:
+        f.write(json.dumps(seasonSummary))
+    f.close()
+
+
+
+file = open("Seasons\\26\\Links.txt", "r")
+episodeLinks = file.read().split("\n")
+file.close()
+
+episodeNumber = 1
+episodeTitles = []
 
 for episode in episodeLinks:
-    time.sleep(2)
     characterData = dict()
     dialogue = []
 
@@ -61,9 +62,9 @@ for episode in episodeLinks:
     titleFile = ("".join([str(episodeNumber), "-", title]))#Modify it to be the format of the output text file
     if(':' in titleFile): titleFile = titleFile.replace(':', '')#In case the title has a ':' in it, remove it since file names can't have colons in names
     episodeTitles.append(titleFile)
-    print(titleFile)
 
-    episodeNumber += 1
+    print(titleFile)
+    episodeNumber += 1#Keep count so the files names maintain episode order
 
     lines = soup.find_all("tr")#Everything in the script is contained in <tr> elements
     for l in lines:
@@ -79,77 +80,41 @@ for episode in episodeLinks:
                 s = s[:start]#Substring til the end cause all instances we've come across have it ending the text
             else:
                 s = ("".join([s[:start], s[end:]]))#Substring from start to opening of scene description, with end of scene description to end of string   s[:start] + s[end:]
-        lowered = s.lower().replace("-", " ")#Replace hyphens with space so words or stutters(F Jimmy) don't get lost
+        lowered = s.lower().replace("-", " ")#Replace hyphens with space so words or stutters(Fucking Jimmy) don't get lost
         removedPunctuation = re.sub(r'[^\w\s]', '', lowered)#Remove punctuation from stirng
 
         characterAndDialogue = removedPunctuation.split("\n")#Format result to just be character name and dialogue, no \n. Split at new line so character name is first and what they say is second.
-        while("" in characterAndDialogue): 
-            characterAndDialogue.remove("")#Remove empty elements in list
+        characterAndDialogue[:] = [x for x in characterAndDialogue if x]#Remove empty elements in list
 
         if(len(characterAndDialogue) == 1): characterAndDialogue.append('i')#Very rare cases where the only "dialogue" on the script is an action in the '[]', so there would be no actual words to analyze
 
         lineWordProf = analyzeLine(characterAndDialogue[1])#Get the data for each line
 
         if(characterAndDialogue[0] in characterData):#If character already has an entry in the dictionary
-            for i in range(0,3):
+            for i in range(0,4):
                 characterData[characterAndDialogue[0]][i] += lineWordProf[i]#Increment the existing elements by the data found in the current line
-            if(lineWordProf[3] != None):#If the profanity frequency dictionary is not empty
-                characterData[characterAndDialogue[0]][3] += lineWordProf[3]#Add the profanities from the line we just analyzed
         else:
             characterData[characterAndDialogue[0]] = lineWordProf#Create a new entry and set it to the data found in the current line
 
     sortedCharacterDataByLines = dict(sorted(characterData.items(), key=operator.itemgetter(1), reverse=True))#Sort dictionary so character with most lines is first
 
-    names = sortedCharacterDataByLines.keys()#Temp variable
+    names = sortedCharacterDataByLines.keys()#Temp variable containing the keys (character names)
     for k in names:
         values = sortedCharacterDataByLines.get(k)
-        values.append(round(values[2]/values[1] * 100, 2))
-        values[3] = dict(Counter(values[3]).most_common())
+        values.append(round(values[2]/values[1] * 100, 2))#Append the frequency of profanities as a %. Profanities/Words 
+        values[3] = dict(Counter(values[3]).most_common())#Count the frequency of each profanity. Replace the list in the character data with a dictionary that has profanity as key and frequency as value
         
-    for i in range(len(sortedCharacterDataByLines)):
-        t = sortedCharacterDataByLines.keys()#Temp variable
-        # t.append(round(t[2]/t[1] * 100, 2))#Append the frequency of profanities as a %. Profanities/Words
-        # t[3] = OrderedDict(Counter(t[3]).most_common())#Count the frequency of each profanity. Replace the list in the character data with a dictionary that has profanity as key and frequency as value
+    with open('Seasons\\26\\' + titleFile + '.json', 'w') as f:
+        f.write(json.dumps(sortedCharacterDataByLines))#Write the episode data to json file
+    
+    time.sleep(2)
 
-    with open('Seasons\\26\\' + titleFile + '.json', 'w') as convert_file:
-        convert_file.write(json.dumps(sortedCharacterDataByLines))
 
-convert_file.close()
+f.close()
 computeSummary()
 
-# seasonSummary = dict()
-
-# for e in episodeTitles:#Subsequent episodes and their data
-#     current =  json.load(open('Seasons\\26\\' + e + '.json'))#Get the data of this episode
-#     for c in current:#For each key
-#         if(c in seasonSummary.keys()):#If character already has an entry in the dictionary, so if they were in one of previous episodes
-#             for j in range(0,3):
-#                 seasonSummary[c][j] += current[c][j]#Update lines, words, and profanity count
-#             seasonSummary[c][4] = round(seasonSummary[c][2]/seasonSummary[c][1] * 100, 2)#Update the profanity % with updated words and profanity count data
-#             seasonSummary[c][3] = dict(sorted((Counter(seasonSummary[c][3]) + Counter(current[c][3])).items(), key=operator.itemgetter(1), reverse=True))#Merge the profanity frequency dictionaries and sort in descending order 
-#         else:
-#             seasonSummary[c] = current[c]#Make a new entry
-
-# seasonSummary = dict(sorted(seasonSummary.items(), key=operator.itemgetter(1), reverse=True))#Sort dictionary so character with most lines is first
-
-# print(seasonSummary)
-
-    #         print(c[0])
-
-        #     for i in range(0,3):
-                # print(c[1][i])
-        #         first[c[1]][i] += c[i]#Increment the existing elements by the data found in the current line
-        #     if(lineWordProf[3] != None):#If the profanity frequency dictionary is not empty
-        #         characterData[characterAndDialogue[0]][3] += lineWordProf[3]#Add the profanities from the line we just analyzed
-        # else:
-        #     characterData[characterAndDialogue[0]] = lineWordProf#Create a new entry and set it to the data found in the current line
-
-
-
-
-    # first = first + next
-
-# print(first)
+# et = time.time()
+# print((et-st)*1000)
 
 
 # Bugs to fix
@@ -174,3 +139,7 @@ computeSummary()
 
     # for i in range(len(sortedCharacterDataByLines)):
     #     sortedCharacterDataByLines[i][1][3] = dict(sorted(sortedCharacterDataByLines[i][1][3].items(), key=operator.itemgetter(1), reverse=True))#For each character, sort their profanity frequency dictionary so most common used ones are first 
+
+
+            # if(lineWordProf[3] != None):#If the profanity frequency dictionary is not empty
+            #     characterData[characterAndDialogue[0]][3] += lineWordProf[3]#Add the profanities from the line we just analyzed
