9d9205ddb6fbb8231ace2885a472c9c51ddc33ec
MasLuarasi <87911434+MasLuarasi@users.noreply.github.com>
2023-04-19 12:33:16 -0400

    Finished seriesSummary

--- seriesSum.py
+++ seriesSum.py
@@ -5,11 +5,11 @@ import json
 import operator
 
 
-def computeSeriesSummary():#Add all the data from each episode in the season
-    directory = 'Series/'
+def computeSeriesSummary():#Add all the data from each season summary into a series summary
+    directory = 'Series/'#Navigate to folder
     seriesSummary = dict()
-    for filename in os.listdir(directory):
-        current =  json.load(open(os.path.join(directory, filename)))#Get the data of current episode
+    for filename in os.listdir(directory):#For each seasonSummary in the directory
+        current = dict(json.load(open(os.path.join(directory, filename))))#Get the data of current seasonSummary
         for c in current:#For each key
             if(c in seriesSummary.keys()):#If character already has an entry in the dictionary, so if they were in one of previous episodes
                 for j in range(0,3):
@@ -22,26 +22,7 @@ def computeSeriesSummary():#Add all the data from each episode in the season
     seriesSummary = dict(sorted(seriesSummary.items(), key=lambda x: x[1][0], reverse=True))#Sort dictionary so character with most lines is first
 
     with open('Series/Summary.json', 'w') as f:
-        f.write(json.dumps(seriesSummary))#Create the season summary json file with the contents in the dictionary
+        f.write(json.dumps(seriesSummary))#Create the series summary json file with the contents in the dictionary
     f.close()
 
 computeSeriesSummary()
-
-# # Path to the directory containing the JSON files
-# path_to_files = 'Series/'
-
-# # Create an empty dictionary to store the merged contents
-# merged_data = {}
-
-# # Loop through each file in the directory
-# for filename in os.listdir(path_to_files):
-#     if filename.endswith('.json'):  # Only consider JSON files
-#         # Open the file and load its contents as a dictionary
-#         with open(os.path.join(path_to_files, filename)) as f:
-#             file_data = json.load(f)
-#         # Merge the file's contents into the main dictionary
-#         merged_data.update(file_data)
-
-# # Save the merged contents to a new JSON file
-# with open('Series/Summary.json', 'w') as f:
-#     json.dump(merged_data, f)
