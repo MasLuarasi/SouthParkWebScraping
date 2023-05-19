d0656196e52d00c6e375d8cc7ea9d81c47ff37f1
MasLuarasi <87911434+MasLuarasi@users.noreply.github.com>
2023-05-19 14:10:01 -0400

    Modified Season 26
    Can run it on all seasons, but want to make sure this new format is easier to work with

--- modifyJSON.py
+++ modifyJSON.py
@@ -1,21 +1,25 @@
+import os
 import json
 
-json_file_path = "Movie\Test.json"
 
-with open(json_file_path, "r") as json_file:
-    data = json.load(json_file)
+def modifyJSON():#Add all the data from each season summary into a series summary
+    directory = "Seasons\\26"#Navigate to folder
+    for filename in os.listdir(directory):#For each JSON in the directory
+        file_path = os.path.join(directory, filename)#Get file path for reading/writing purposes
+        if(filename.endswith(".json")):#If JSON file
+            with open(file_path, "r") as f:
+                data = json.load(f)
+            modified_data = {}#Dictionary for modified data
+            for key, values in data.items():#For k,v in dictionary
+                modified_entry = {#Add identifiers
+                    "Lines": values[0],
+                    "Words": values[1],
+                    "Profanity Count": values[2],
+                    "Profanity List": values[3],
+                    "Profanity Frequency": values[4]
+                }
+                modified_data[key] = modified_entry#Add the key value pair with modified value entry to the output dictionary
+            with open(file_path, "w") as f:
+                json.dump(modified_data, f)#Rewrite with modifications
 
-for entry in data:
-    name = entry[0]
-    values = entry[1]
-    modified_data = [name, {
-        "Lines": values[0],
-        "Words": values[1],
-        "Profanity Count": values[2],
-        "Profanity List": values[3],
-        "Profanity Frequency": values[4]
-    }]
-    data[data.index(entry)] = modified_data
-
-with open(json_file_path, "w") as json_file:
-    json.dump(data, json_file)
+modifyJSON()
