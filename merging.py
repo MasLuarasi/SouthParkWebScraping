44186aed834de30df8afcbccf92c675920a05e17
MasLuarasi <87911434+MasLuarasi@users.noreply.github.com>
2023-08-10 15:04:56 -0400

    Minor changes

--- merging.py
+++ merging.py
@@ -6,7 +6,6 @@ characterAlias = [
                 ['mayor mcdaniels', 'mayor mcdaneils', 'mayor mcdanniels', 'mayor']
                 ]           
                 
-
 with open('Series\\Summary.json', 'r') as file:#Will be set to series summary or go through each episode
     data = json.load(file)
 
@@ -23,21 +22,24 @@ def mergeDicts(*dicts):#Looking at profanity count dictionaries for a given char
 
 def combineEntries(characterList):
     lines, words, profanities, profCount = 0, 0, 0, {}
-    for d in characterList: 
+    for d in characterList:
         if d in data:#If this name is in the dataset
             lines += data[d][0]#Increment each of these values
             words += data[d][1]
             profanities += data[d][2]
             profCount = mergeDicts(profCount, data[d][3])#Merge the profanity count dictionaries into one
-            data.pop(d)#Remove their entry from the dataset
-    combinedDict = [lines, words, profanities, dict(Counter(profCount).most_common()), (round(profanities/words * 100, 2))]#Put all the data together
-    return combinedDict
+    return [lines, words, profanities, dict(Counter(profCount).most_common()), (round(profanities/words * 100, 2))]#Return all the data together
 
 for c in characterAlias:
-    combinedData = combineEntries(c)
-    data[c[0]] = combinedData#Assign the single entry in the dataset to the returned combined data
+#    combinedData = combineEntries(c) Keep for potential debugging purposes
+    data[c[0]] = combineEntries(c)#Assign the single entry in the dataset to the returned combined data
 
-data = dict(sorted(data.items(), key=lambda x: x[1][0], reverse=True))#Sort dictionary so character with most lines is first
+for c in characterAlias:
+    for a in range(1, len(c)):
+        try: data.pop(c[a])
+        except: continue
+
+data = dict(sorted(data.items(), key=lambda x: x[1][0], reverse=True))#Sort dictionary after all changes so character with most lines is first
 
 with open('Z.json', 'w') as f:
     f.write(json.dumps(data))
@@ -45,8 +47,6 @@ with open('Z.json', 'w') as f:
 #NEXT STEPS
 #Make lists for all the characters and the entries we have to look for for each one
 #Reorganize the code to sort through them
-#Remove the pop statement from the loop, we're gonna pop everything we don't need at the end
-#See how to get the name we want (first in each list) an assign that as the replacement name
 
 # cartman # kyle cartman # eric cartman
 # kyle # kyle broflovski # kyle cartman # kyle ms crabtree
@@ -78,3 +78,12 @@ with open('Z.json', 'w') as f:
 #     temp[4] = 0#just add 0 to avoid divide by zero error
 # else:
 #     temp[4] = (round(temp[2]/temp[1] * 100, 2))#Append how often character uses profanity as a %. Profanities/Words 
+
+
+# for index in range(1, 2):
+#     filePath = os.path.join('Seasons', str(index))#'Seasons\1...
+#     if os.path.exists(filePath):
+#         epFiles = natsorted(glob.glob(os.path.join(filePath, '*.json')))#Gather all the json files
+#         for ep in epFiles:#For each episode in this season
+#             with open(ep, 'r') as file:
+#                 data = json.load(file)
